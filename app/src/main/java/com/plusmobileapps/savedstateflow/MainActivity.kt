98586569941eec05d0c7cf21d076435cde0ae1cd
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-12-24 03:40:13 -0800

    create SavedStateFlow and use in sample

--- app/src/main/java/com/plusmobileapps/savedstateflow/MainActivity.kt
+++ app/src/main/java/com/plusmobileapps/savedstateflow/MainActivity.kt
@@ -3,16 +3,26 @@ package com.plusmobileapps.savedstateflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.plusmobileapps.savedstateflow.ui.theme.SavedStateFlowTheme
+import kotlinx.coroutines.flow.StateFlow
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ExampleContent(viewModel)
                 }
             }
         }
@@ -30,14 +40,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ExampleContent(viewModel: MainViewModel) {
+    Scaffold {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            QueryTextField(stateFlow = viewModel.query, onValueChanged = viewModel::queryUpdated)
+            LoadingUI(stateFlow = viewModel.isLoading)
+            NewsArticlesUI(stateFlow = viewModel.newsArticles)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SavedStateFlowTheme {
-        Greeting("Android")
+fun QueryTextField(stateFlow: StateFlow<String>, onValueChanged: (String) -> Unit) {
+    val state = stateFlow.collectAsState()
+    OutlinedTextField(
+        modifier = Modifier.padding(8.dp),
+        value = state.value,
+        label = { Text("Query") },
+        onValueChange = onValueChanged
+    )
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun LoadingUI(stateFlow: StateFlow<Boolean>) {
+    val state = stateFlow.collectAsState()
+    AnimatedVisibility(modifier = Modifier.padding(8.dp), visible = state.value) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun NewsArticlesUI(stateFlow: StateFlow<List<String>>) {
+    val state = stateFlow.collectAsState()
+    LazyColumn(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        items(state.value.size) { index ->
+            val article = state.value[index]
+            Column {
+                Text(text = article)
+                Divider()
+            }
+        }
     }
 }
---
+++ savedstateflow/src/main/java/com/plusmobileapps/savedstateflow/SavedStateFlow.kt
@@ -0,0 +1,63 @@
+package com.plusmobileapps.savedstateflow
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+/**
+ * A [StateFlow] wrapper around [SavedStateHandle.getLiveData].
+ */
+interface SavedStateFlow<T> {
+    var value: T
+
+    fun asStateFlow(): StateFlow<T>
+
+}
+
+/**
+ * Builder function for creating a [SavedStateFlow]
+ *
+ * @param savedStateHandle
+ * @param key
+ * @param initialValue
+ */
+fun <T : Any> ViewModel.SavedStateFlow(savedStateHandle: SavedStateHandle, key: String, initialValue: T): SavedStateFlow<T> =
+    SavedStateFlowImpl(viewModelScope, savedStateHandle, key, initialValue)
+
+private class SavedStateFlowImpl<T>(
+    private val scope: CoroutineScope,
+    private val savedStateHandle: SavedStateHandle,
+    private val key: String,
+    defaultValue: T
+) : SavedStateFlow<T> {
+    private val _state: MutableStateFlow<T> =
+        MutableStateFlow(savedStateHandle.get<T>(key) ?: defaultValue)
+
+    init {
+        observeSavedState()
+    }
+
+    override var value: T
+        get() = _state.value
+        set(value) {
+            savedStateHandle.set(key, value)
+        }
+
+    override fun asStateFlow(): StateFlow<T> = _state
+
+    private fun observeSavedState() {
+        scope.launch {
+            savedStateHandle.getLiveData<T>(key).asFlow().collect { value ->
+                _state.value = value
+            }
+        }
+    }
+
+}
