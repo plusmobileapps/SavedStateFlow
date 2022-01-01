9faa4d76db524acd743eee38a879f11296116dcf
Andrew Steinmetz <andrew@plusmobileapps.com>
2022-01-01 14:37:29 -0800

    write one test with the stateflow handle

--- sample/src/test/java/com/plusmobileapps/savedstateflow/MainViewModelTest.kt
+++ sample/src/test/java/com/plusmobileapps/savedstateflow/MainViewModelTest.kt
@@ -1,36 +1,42 @@
 package com.plusmobileapps.savedstateflow
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 class MainViewModelTest {
-
-    @get:Rule
-    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
     lateinit var viewModel: MainViewModel
 
-    private val savedStateHandle: SavedStateHandle = mockk()
+    private val savedStateHandle: SavedStateFlowHandle = mockk()
     private val newsDataSource: NewsDataSource = mockk()
+    private val savedStateFlow: SavedStateFlow<String> = TestSavedStateFlow<String>("")
 
     @Before
     fun setUp() {
+        every { newsDataSource.fetchQuery("") } returns flow {  }
+        every {
+            savedStateHandle.getSavedStateFlow(
+                any(),
+                MainViewModel.SAVED_STATE_QUERY_KEY,
+                ""
+            )
+        } returns savedStateFlow
         Dispatchers.setMain(mainThreadSurrogate)
         viewModel = MainViewModel(savedStateHandle, newsDataSource)
     }
@@ -42,17 +48,29 @@ class MainViewModelTest {
     }
 
     @Test
-    fun `saved state flow returns cache value and fetches results`() = runTest {
+    fun `saved state flow returns cache value and fetches results`() = runBlocking {
         val newQuery = "some new query"
-        val expected = listOf<String>("some value", "some second value")
-        every { savedStateHandle.get<String>(MainViewModel.SAVED_STATE_QUERY_KEY) } returns null
-        every { savedStateHandle.getLiveData<String>(MainViewModel.SAVED_STATE_QUERY_KEY) } returns MutableLiveData("")
-        every { newsDataSource.fetchQuery(newQuery) } returns flow {
-            emit(expected)
+        val results = listOf<String>("some value", "some second value")
+        val expectedState = MainViewModel.State(false, newQuery, results)
+        every { newsDataSource.fetchQuery(newQuery) } returns flowOf(results)
+
+        viewModel.state.test {
+            viewModel.updateQuery(newQuery)
+            assertEquals(MainViewModel.State(false, "", emptyList()), awaitItem())
+            assertEquals(MainViewModel.State(true, newQuery, emptyList()), awaitItem())
+            assertEquals(expectedState, awaitItem())
         }
-
-        viewModel.updateQuery(newQuery)
-
-        assertEquals(expected, viewModel.state.value.results)
     }
+}
+
+/**
+ * TODO move into its own test module
+ */
+class TestSavedStateFlow<T : Any>(defaultValue: T, cachedValue: T? = null) : SavedStateFlow<T> {
+
+    private val stateFlow = MutableStateFlow(cachedValue ?: defaultValue)
+
+    override var value: T by stateFlow::value
+
+    override fun asStateFlow(): StateFlow<T> = stateFlow
 }
