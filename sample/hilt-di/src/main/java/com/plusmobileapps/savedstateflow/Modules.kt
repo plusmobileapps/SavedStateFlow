9c32ff66d737e242cc02e725be3e22e441bd3389
Andrew Steinmetz <andrew@plusmobileapps.com>
2022-01-08 09:34:37 -0800

    add assisted viewmodel extension functions and update the documentation

--- sample/hilt-di/src/main/java/com/plusmobileapps/savedstateflow/Modules.kt
+++ sample/hilt-di/src/main/java/com/plusmobileapps/savedstateflow/Modules.kt
@@ -1,12 +1,8 @@
 package com.plusmobileapps.savedstateflow
 
-import androidx.lifecycle.SavedStateHandle
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 
 @Module
